eb87c409a15abd866ad9e0ad0c09ce37432070ab
SherlockCat <xi_afan@163.com>
2016-08-22 17:13:22 +0800

    8.22

--- SecondPageComponent.js
+++ SecondPageComponent.js
@@ -3,21 +3,55 @@ import  React, {
 } from 'react'
 import {
     View,
-    Text
+    Text,
+    ListView
 } from  'react-native'
 
 class SecondPageComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+        };
+    }
+
+    componentWillMount() {
+        this.genRows();
     }
 
     render() {
-        return (
-            <View>
-                <Text style={{fontSize: 20}}>第二个页面</Text>
-            </View>
-        );
+        if (this.state.dataSource.getRowCount() === 0) {
+            return (
+                <View >
+                    <Text>
+                        加载中....
+                    </Text>
+                </View>
+            );
+        } else {
+            return (
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) =><Text>{rowData.desc}</Text>
+                    }>
+                </ListView>
+            );
+        }
+
+    }
+
+    genRows() {
+        this.getAndroidGank();
+    }
+
+    getAndroidGank() {
+        fetch("http://gank.io/api/day/2015/08/06")
+            .then((response)=>response.json)
+            .then((responseJson)=> {
+                if (responseJson.results) {
+                    this.setState({dataSource: responseJson.results});
+                }
+            }).catch((error)=>console.error(error));
     }
 }
 
